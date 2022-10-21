63de398a84e6a767a93d8ba79c1fc6a0dbf0d90b
Lizardo10 <lizardoperezjimenez@gmail.com>
2022-10-20 18:10:19 -0600

    Actualizacion de la pagina de inicio

--- pages/api/auth/sign_up.js
+++ pages/api/auth/sign_up.js
@@ -2,16 +2,16 @@
 //dbConnect();
 import joi from "@hapi/joi";
 const schemaRegister = joi.object({
-  name:Joi.string().min(3).max(255).required(),
-  email: Joi.string().min(6).max(255).required().email(),
-  name: Joi.string().min(8).max(1024).required(),
+  name: joi.string().min(3).max(255).required(),
+  email: joi.string().min(6).max(255).required().email(),
+  name: joi.string().min(8).max(1024).required(),
 })
 export default function handlerSign_up(req, res) {
   const { body, method } = req;
   if (method == "POST") {
     const { name, email, password } = body;
     console.log(name, email, password);
-    res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({ name, email, password })
   }
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 export default function index() {
-  return(
+  return (
     <div>
-      <h1>1
-      </h1>
+      <div>
+        <h1>Que es un grafo</h1>
+        <p>Un grfo es un susecion de aristas y vectores</p>
+      </div>
+
     </div>
   )
 };
